caa07ad7901c081da9299e6992369b62df4d1e2f
Bogdan <vasielscubogdan1@gmail.com>
2024-07-06 15:40:40 +0300

    TestUserRepository

--- Server/src/main/java/ace/ucv/messenger/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package ace.ucv.messenger.repository;
-
-import ace.ucv.messenger.entity.Message;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MessageRepository extends MongoRepository<Message, ObjectId> {
-}
